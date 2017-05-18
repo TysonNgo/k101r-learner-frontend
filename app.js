0d5874e44034c3c836117346f769d65fb695fe40
T <tyson_ngo@hotmail.com>
2017-05-18 01:44:40 -0400

    refactored views directory

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.set('view options', { basedir: __dirname})
 app.use(express.static(__dirname + '/static'));
 
 var port = process.env.PORT || 8080;
@@ -11,6 +12,18 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+router.get('/dictation', (req, res) => {
+	res.render('dictation/index');
+});
+
+router.get('/translate', (req, res) => {
+	res.render('translate');
+});
+
+router.get('/grammar', (req, res) => {
+	res.render('grammar');
 });
 
 app.use('/', router);
