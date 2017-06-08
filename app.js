2f5a7a5c0d44ce9ba29dc87baf7704627d3fb797
T <tyson_ngo@hotmail.com>
2017-06-08 06:35:03 -0400

    fixed lessons 6-10 in vocab notes

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ router.get('/exercise/:exercise', (req, res) => {
 				{id:3, title: 'Lesson 3'},
 				{id:4, title: 'Lesson 4'},
 				{id:5, title: 'Lesson 5'},
-				{id:5, title: 'Lesson 6'},
-				{id:5, title: 'Lesson 7'},
-				{id:5, title: 'Lesson 8'},
-				{id:5, title: 'Lesson 9'},
-				{id:5, title: 'Lesson 10'}
+				{id:6, title: 'Lesson 6'},
+				{id:7, title: 'Lesson 7'},
+				{id:8, title: 'Lesson 8'},
+				{id:9, title: 'Lesson 9'},
+				{id:10, title: 'Lesson 10'}
 			]; break;
 		case 'translate':
 			var exercises = [
@@ -62,11 +62,11 @@ router.get('/notes/:id', (req, res) => {
 					{id:3, title: 'Lesson 3'},
 					{id:4, title: 'Lesson 4'},
 					{id:5, title: 'Lesson 5'},
-					{id:5, title: 'Lesson 6'},
-					{id:5, title: 'Lesson 7'},
-					{id:5, title: 'Lesson 8'},
-					{id:5, title: 'Lesson 9'},
-					{id:5, title: 'Lesson 10'}
+					{id:6, title: 'Lesson 6'},
+					{id:7, title: 'Lesson 7'},
+					{id:8, title: 'Lesson 8'},
+					{id:9, title: 'Lesson 9'},
+					{id:10, title: 'Lesson 10'}
 				]
 			});
 		case '4':
